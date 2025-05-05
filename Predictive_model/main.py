51e157cbc609e710437c4a525ead4a12e98e2dd4
Maheshwaran G <maheshwarang16@gmail.com>
2025-05-05 15:38:15 +0530

    host modified

--- Predictive_model/main.py
+++ Predictive_model/main.py
@@ -15,4 +15,4 @@ def remove_session(exception=None):
 app.register_blueprint(routes.routes)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
